575037a0dd17038970617e034141484768dab628
hackerspace-Johan <johansokrates@hotmail.com>
2015-11-14 17:47:54 +0100

    Fix wrapper destructor bug

--- src/wrapper.hpp
+++ src/wrapper.hpp
@@ -9,29 +9,30 @@ static const int depth_width = 512, depth_height = 424, color_width = 1920, colo
 
 struct MyKinect {
   Freenect2 freenect2;
-  SyncMultiFrameListener listener;
+  SyncMultiFrameListener*listener;
   FrameMap frames;
   int gotFrame;
   Freenect2Device*dev;
-  MyKinect() :
-    listener(SyncMultiFrameListener(Frame::Depth|Frame::Color)){
+  MyKinect() {
+    listener = new SyncMultiFrameListener(SyncMultiFrameListener(Frame::Depth|Frame::Color));
     freenect2.enumerateDevices();
     std::string serial = freenect2.getDefaultDeviceSerialNumber();
     dev = freenect2.openDevice(serial);
-    dev->setColorFrameListener(&listener);
-    dev->setIrAndDepthFrameListener(&listener);
+    dev->setColorFrameListener(listener);
+    dev->setIrAndDepthFrameListener(listener);
     dev->start();
     gotFrame = 0;
   }
   ~MyKinect() {
     dev->stop();
     dev->close();
+    delete listener;
   }
   void getColorAndDepth(unsigned int**col, float**depth) {
     if (gotFrame)
-      listener.release(frames);
+      listener->release(frames);
 
-    listener.waitForNewFrame(frames);
+    listener->waitForNewFrame(frames);
     gotFrame = 1;
 
     *depth = (float*)frames[Frame::Depth]->data;
