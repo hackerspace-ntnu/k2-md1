eda2dc670813c25276c657d8460fbe1e1ca07b98
Johan <johansokrates@hotmail.com>
2015-11-14 16:10:42 +0100

    Make clinit look for GPUs instead of blindly assuming some platform

--- src/cl/clinit.hpp
+++ src/cl/clinit.hpp
@@ -11,12 +11,18 @@ static cl::CommandQueue queue;
 void initCL() {
   try {
     std::vector<cl::Platform> platforms;
-    // create platform
     cl::Platform::get(&platforms);
-    platforms[0].getDevices(CL_DEVICE_TYPE_ALL, &devices);
-    // create context
+		for (int i = 0; i < platforms.size(); i++) {
+			try {
+				platforms[i].getDevices(CL_DEVICE_TYPE_GPU, &devices);
+				cout << "Using platform #" << i+1 << endl;
+				break;
+			} catch (cl::Error e) {
+				if (e.err() == CL_DEVICE_NOT_FOUND) continue; //No GPU on this platform, try next
+				std::cout << std::endl << e.what() << " : " << e.err() << std::endl;
+			}
+		}
     context = cl::Context(devices);
-    // create command queue
     queue = cl::CommandQueue(context, devices[0]);
   } catch (cl::Error e) {
     std::cout << std::endl << e.what() << " : " << e.err() << std::endl;
@@ -27,16 +33,11 @@ cl::Program createProgram(std::string name) {
   std::ifstream cl_file(name.c_str());
   std::string cl_string(std::istreambuf_iterator<char>(cl_file), (std::istreambuf_iterator<char>()));
   cl::Program::Sources source(1, std::make_pair(cl_string.c_str(), cl_string.length() + 1));
-  // create program
   cl::Program program(context, source);
-  // compile opencl source
   try {
     program.build(devices);
   } catch (cl::Error&e) {
-    //std::cout << "Build Status: " << program.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(devices[0]) << std::endl;
-    //std::cout << "Build Options:\n" << program.getBuildInfo<CL_PROGRAM_BUILD_OPTIONS>(devices[0]) << std::endl;
     std::cout << "Build Error:\n" << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices[0]) << std::endl;
-    //throw std::runtime_error(std::string("Build Error:")+program.getBuildInfo<CL_PROGRAM_BUILD_LOG >(devices[0]));		
   }
   return program;
 }
