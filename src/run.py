69726434af159cec19c8528ffeb4197ae95b120b
Hackerspace <hackerspace@hackerspace>
2016-02-15 18:59:20 +0100

    Remove sudo from run.py

--- src/run.py
+++ src/run.py
@@ -4,6 +4,6 @@ from sys import argv
 if len(argv) < 2:
     print ("Usage: ./run.py file.cpp")
 else:
-    command = "g++ %s -lX11 -Wl,-rpath=/home/hackerspace/Documents/k2-md1/deps/libfreenect2/build/lib -L /home/hackerspace/Documents/k2-md1/deps/libfreenect2/build/lib -lfreenect2 -I /home/hackerspace/Documents/k2-md1/deps/libfreenect2/include -O3 && sudo ./a.out"%argv[1]
+    command = "g++ %s -lX11 -Wl,-rpath=/home/hackerspace/Documents/k2-md1/deps/libfreenect2/build/lib -L /home/hackerspace/Documents/k2-md1/deps/libfreenect2/build/lib -lfreenect2 -I /home/hackerspace/Documents/k2-md1/deps/libfreenect2/include -O3 && ./a.out"%argv[1]
     print (command)
     system(command)
