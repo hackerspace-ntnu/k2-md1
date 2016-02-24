1935baacfdf5a0e117a6beab843c9775c6e5a14f
hbirchtree <h0wy36@gmail.com>
2016-02-24 19:24:45 +0100

    - Remove old Makefiles

--- src/run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-from os import system
-from sys import argv
-if len(argv) < 2:
-    print ("Usage: ./run.py file.cpp")
-else:
-    command = "g++ %s -lX11 -Wl,-rpath=/home/hackerspace/Documents/k2-md1/deps/libfreenect2/build/lib -L /home/hackerspace/Documents/k2-md1/deps/libfreenect2/build/lib -lfreenect2 -I /home/hackerspace/Documents/k2-md1/deps/libfreenect2/include -O3 && ./a.out"%argv[1]
-    print (command)
-    system(command)
